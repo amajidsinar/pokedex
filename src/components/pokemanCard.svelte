6dcd09f04d5ef53b548a8f35a724572ef826234a
jedi <amajidsinar@gmail.com>
2021-10-09 20:40:11 +0700

    add image and name for each pokemon

--- src/components/pokemanCard.svelte
+++ src/components/pokemanCard.svelte
@@ -1,4 +1,7 @@
 <script>
     export let pokeman;
 </script>
-{pokeman.name}
+<a href={`/pokeman/${pokeman.id}`}>
+    <img src={pokeman.image} alt={pokeman.name}>
+    <h2>{pokeman.id}. {pokeman.name}</h2>
+</a>
