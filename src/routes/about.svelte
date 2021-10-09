bb9ee72d27231ac75a4d7c29142d8ec653135977
jedi <amajidsinar@gmail.com>
2021-10-09 19:38:14 +0700

    update navbar and header layout

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,4 +1,4 @@
-<h1>About</h1>
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
 <p>THis is a really cool Svelte Kit demo that uses the Pokemon API to build a Pokedex</p>
 <svelte:head>
     <title>Svelte Kit Pokedex</title>
