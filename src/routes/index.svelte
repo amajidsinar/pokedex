37e726821500b667ccf2e406c8536c7afdd9172e
jedi <amajidsinar@gmail.com>
2021-10-09 20:31:48 +0700

    initial layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
 <script>
-    import {pokemon} from "../stores/pokemart"
+    import {pokemon} from "../stores/pokemart";
+    import PokemanCard from "../components/pokemanCard.svelte";
+
 </script>
 <svelte:head>
     <title>Svelte Kit Pokedex</title>
@@ -8,7 +10,7 @@
 <h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
 
 {#each $pokemon as pokeman}
-<p>{pokeman.name}</p>
+    <PokemanCard pokeman={pokeman}/>
 {/each}
 
 <style>
